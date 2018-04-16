67edc4deca95fd7e5014333733fd0989b18632d0
koshikraj <koshik.raj@gmail.com>
2018-04-16 15:23:37 +0530

    adds youtube api

---
+++ youtube_api.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# This sample executes a search request for the specified search term.
+# Sample usage:
+#   python search.py --q=surfing --max-results=10
+# NOTE: To use the sample, you must provide a developer key obtained
+#       in the Google APIs Console. Search for "REPLACE_ME" in this code
+#       to find the correct place to provide that key..
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = 'REPLACE ME'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+max_results = 5
+
+def youtube_search(query):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=query,
+        part='id,snippet',
+        maxResults=max_results
+    ).execute()
+
+    videos = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append({'title': search_result['snippet']['title'],
+                           'id': search_result['id']['videoId']})
+
+    return videos
+
+
+
